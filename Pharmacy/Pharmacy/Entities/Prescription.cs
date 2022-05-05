2b83bbf1382403949c9e1c5845fd9252c8861800
Bruno Kozakowski-Kędzierski <s23719@pjwstk.edu.pl>
2022-05-05 11:46:12 +0200

    init

---
+++ Pharmacy/Pharmacy/Entities/Prescription.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pharmacy.Entities
+{
+    public class Prescription
+    {
+        public int IdPrescription { get; set; }
+        public DateTime Date { get; set; }
+
+        public ICollection<PrescriptionMedicament> PrescriptionsMedicaments { get; set; }
+
+
+        public Prescription() {
+
+            PrescriptionsMedicaments = new HashSet<PrescriptionMedicament>();
+        
+        }
+    }
+}
---
+++ Pharmacy/Pharmacy/Entities/PrescriptionMedicament.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pharmacy.Entities
+{
+    public class PrescriptionMedicament
+    {
+        public int IdMedicament { get; set; }
+        public int IdPrescription { get; set; }
+        public int Dose { get; set; }
+
+        public virtual Medicament IdMedicamentNavigation { get; set; }
+        public virtual Prescription IdPrescriptionNavigation { get; set; }
+
+    }
+}
